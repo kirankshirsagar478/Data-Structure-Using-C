ffeaaeae9a619d14a7b6202b0aad74037d865960
kirankshirsagar478 <kirankshirsagar478@gmail.com>
2022-09-22 23:42:10 +0530

    correction in stack_operations.c

--- stack_operations.c
+++ stack_operations.c
@@ -1,30 +1,98 @@
-#include<stdio.h>
-#include<conio.h>
+//  prog on implementation of stack using array//
 
-struct ss
+#include <stdio.h>
+#include <conio.h>
+//#include<cruses.h>
+#define SIZE 10
+struct stack
 {
-    char stack[5];
-    int top;
-}s;
-void initialize ()
+  int s[SIZE];
+  int top;
+} st;
+int stfull()
 {
-    s.top=-1;
-}
-int empty ()
-{
-    if(s.top==1)
+  if (st.top >= SIZE - 1)
     return 1;
+  else
     return 0;
 }
-int full ()
+void push(int item)
 {
-    int size;
-    if(s.top==size-1)
+  st.top++;
+  st.s[st.top] = item;
+}
+int stempty()
+{
+  if (st.top == -1)
     return 1;
+  else
     return 0;
 }
-void main()
+int pop()
 {
-    getch();
-    
+  int item;
+  item = st.s[st.top];
+  st.top--;
+  return (item);
+}
+void display()
+{
+  int i;
+  if (stempty())
+    printf("\tstack empty");
+  else
+  {
+    for (i = st.top; i >= 0; i--)
+      printf("\n %d", st.s[i]);
+  }
+}
+//void main()
+int main()
+{
+
+  int item, choice;
+  char ans;
+  //clrscr();
+  st.top = -1;
+
+  do
+  {
+
+    printf("\n1.push\n2.pop\n3.display\n4.exit");
+    printf("\nEnter choice::");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+      printf("\n Enter element to be push::");
+      scanf("%d", &item);
+      if (stfull())
+        printf("\t stack is full");
+      else
+        push(item);
+      break;
+    case 2:
+      printf("poped element::");
+      if (stempty())
+        printf("\t\t stack is empty");
+      else
+      {
+        item = pop();
+        printf("element is %d", item);
+      }
+      break;
+    case 3:
+      display();
+      break;
+    case 4:
+      //exit(0);
+      break;
+    }
+    printf("\n\n Do U want to continue::");
+    ans = getche();
+  } while (ans == 'y' || ans == 'Y');
+
+  printf("\n Enter choice::");
+  scanf("%d", &choice);
+  //getch();
 }
